cb89ea5d5f88bed637d1457a874fe30ec2f7028b
Damian Nolan <damiannolan@gmail.com>
2017-02-24 02:58:46 +0000

    Added various middleware. CORS working with front-end ng2app

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import * as restify from 'restify';
+import { bootstrap as bootstrapMiddleware } from './middleware';
 import { bootstrap as bootstrapHealthCheck } from './healthcheck';
 
 export const bootstrap = (server: restify.Server) => {
+    bootstrapMiddleware(server);
     bootstrapHealthCheck(server);
 };
---
+++ src/routes/middleware/index.ts
@@ -0,0 +1,36 @@
+import * as bunyan from 'bunyan';
+import * as config from 'config';
+import * as passport from 'passport-restify';
+import * as restify from 'restify';
+import * as jwt from 'restify-jwt';
+
+export const bootstrap = (server: restify.Server) => {
+    const corsHeaders = config.get('Server.corsHeaders') as string[];
+    const corsOrigins = config.get('Server.corsOrigins') as string[];
+
+    // CORS for Cross Domain Requests
+    server.pre(restify.CORS({ headers: corsHeaders, origins: corsOrigins, credentials: true }));
+    server.use(restify.fullResponse());
+    server.use(restify.queryParser());
+    // Accept application/json
+    server.use(restify.acceptParser(server.acceptable));
+    // Authorization parser necessary to take bearer token
+    server.use(restify.authorizationParser());
+    // Passport middleware for facebook login
+    server.use(passport.initialize({ userProperty: 'userAccount' }));
+    // Json web token for authorized routes
+    server.use(jwt(jwtConfig).unless(whitelistConfig));
+    server.use(restify.bodyParser(bodyParserConfig));
+}
+
+const jwtConfig = { secret: config.get('Server.authSecret') };
+
+// Unless on whitelist. middleware will expect you to have a valid jwt to access protected routes
+// Whitelist root /heathcheck
+// And regexp to not have to deal with each individual auth route separately (every route under /auth)
+const whitelistConfig = {
+  path: ['/', '/healthcheck', /(?:auth(?:$|\/\w*?))/i]
+};
+
+const bodyParserConfig = { mapParams: true };
+
